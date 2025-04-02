056f238af510b1ab0ffecd68dfd48ddbb1dcab15
Dev10012003 <dev.dudhia@etatvasoft.com>
2025-04-02 15:53:47 +0530

    update routes in index

--- ems-backend/src/index.ts
+++ ems-backend/src/index.ts
@@ -1,23 +1,21 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import "reflect-metadata";
 import connectToDatabase from "./db/dbConnect.js";
 import authRouter from "./routes/auth.js";
+import dashboardRouter from "./routes/dashboard.js";
 import departmentRouter from "./routes/department.js";
 import employeeRouter from "./routes/employee.js";
-import settingRouter from "./routes/setting.js";
 import leaveRouter from "./routes/leave.js";
 import salaryRouter from "./routes/salary.js";
-import dashboardRouter from "./routes/dashboard.js";
+import settingRouter from "./routes/setting.js";
 import "./utils/salaryScheduler.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const projectRoot = process.cwd();
 
 const app = express();
 
@@ -34,13 +32,11 @@ app.use("/api/dashboard", dashboardRouter);
 
 const PORT = process.env.PORT || 3000;
 
-const root = path.resolve(__dirname, "..");
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(root, "ems-frontend/build")));
+  app.use(express.static(path.join(projectRoot, "ems-frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(root, "ems-frontend", "build", "index.html"));
+    res.sendFile(path.join(projectRoot, "ems-frontend", "build", "index.html"));
   });
 }
 
