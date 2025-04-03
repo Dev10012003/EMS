5c05881cbfec0ce6a7a1cc097536e050cfdf73d6
Dev10012003 <dev.dudhia@etatvasoft.com>
2025-04-03 13:35:47 +0530

    mail changes

--- ems-backend/src/utils/mailer.ts
+++ ems-backend/src/utils/mailer.ts
@@ -11,6 +11,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const sendMail = async (to: string, subject: string, text: string) => {
